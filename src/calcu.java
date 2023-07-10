f60da7a466f4d55d97ea4fb057e26530a51c4396
Alan <alanstvn420@gmail.com>
2023-07-10 18:11:08 -0500

    Initial commit

---
+++ src/calcu.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class calcu extends JFrame {
+    public class
+}
---
+++ src/ventana.java
@@ -0,0 +1,29 @@
+package ventana;
+
+import javax.swing.*;
+
+public class ventana {
+    private JPanel calcu;
+    private JLabel TCalcu;
+    private JButton SENButton;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a1Button;
+    private JButton elevadob;
+    private JButton COSButton;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton a2Button;
+    private JButton a0Button;
+    private JButton TANButton;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton raizb;
+    private JButton Cb;
+    private JButton divb;
+    private JButton restab;
+    private JButton sumab;
+    private JButton igualb;
+    private JLabel Pantalla;
+}
